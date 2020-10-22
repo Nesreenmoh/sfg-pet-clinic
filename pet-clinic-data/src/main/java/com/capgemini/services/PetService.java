369c868a930588e63a868397cd17b0ffb49773e2
Nesreen <nesreenmhd@gmail.com>
2020-10-22 18:28:32 +0200

    Implement Interfaces for Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/capgemini/services/PetService.java
@@ -0,0 +1,12 @@
+package com.capgemini.services;
+
+import com.capgemini.models.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/capgemini/services/VetService.java
@@ -0,0 +1,13 @@
+package com.capgemini.services;
+
+
+import com.capgemini.models.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
