eda541619ee6c6b57a82df945c45101d94809b9e
Nesreen <nesreenmhd@gmail.com>
2020-10-23 17:24:58 +0200

    List all owners on owners index page. Closes #17.

--- pet-clinic-web/src/main/java/com/capgemini/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/capgemini/controllers/OwnerController.java
@@ -1,14 +1,28 @@
 package com.capgemini.controllers;
 
+import com.capgemini.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owner")
 public class OwnerController {
 
+    // define a variable of an owner service
+    private final OwnerService ownerService;
+
+    // constructor
+
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    // retrieve all owners data
     @RequestMapping({"","/","index","index.html"})
-    public String ownerList(){
+    public String ownerList(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
