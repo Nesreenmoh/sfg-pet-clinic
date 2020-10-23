24620d338d930844f468d209231a1ceefc19cab0
Nesreen <nesreenmhd@gmail.com>
2020-10-23 17:33:50 +0200

    List all Vets on Vet index page. Closes #16.

--- pet-clinic-web/src/main/java/com/capgemini/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/capgemini/bootstrap/DataLoader.java
@@ -43,9 +43,9 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2= new Vet();
-        vet1.setId(2L);
-        vet1.setFirstName("Anis");
-        vet1.setLastName("Rashid");
+        vet2.setId(2L);
+        vet2.setFirstName("Anis");
+        vet2.setLastName("Rashid");
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
