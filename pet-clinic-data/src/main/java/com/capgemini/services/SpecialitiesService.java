a168616ff7898cbecaa809b9088f0cf0dc592564
Nesreen <nesreenmhd@gmail.com>
2020-10-25 17:26:42 +0100

    Create Speciality Map Service Impl. Close #32.

---
+++ pet-clinic-data/src/main/java/com/capgemini/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.capgemini.services;
+
+import com.capgemini.models.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/capgemini/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package com.capgemini.services.map;
+
+import com.capgemini.models.Speciality;
+import com.capgemini.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap  extends AbstractMapService<Speciality, Long>implements SpecialitiesService {
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality t) {
+        return super.save(t);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality o) {
+        super.delete(o);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+      super.deleteById(id);
+    }
+}
