981b213285545e0803a23d6987dae6ff956cf0af
Nesreen <nesreenmhd@gmail.com>
2020-10-25 16:42:33 +0100

    Adding PetTypes in with Bootstrap(Startup). Closes #33.

--- pet-clinic-web/src/main/java/com/capgemini/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/capgemini/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.capgemini.bootstrap;
 
 import com.capgemini.models.Owner;
+import com.capgemini.models.PetType;
 import com.capgemini.models.Vet;
 import com.capgemini.services.OwnerService;
+import com.capgemini.services.PetTypeService;
 import com.capgemini.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
    private final OwnerService ownerService;
    private final VetService vetService;
+   private final PetTypeService petTypeService;
 
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedPetDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedPetcatType = petTypeService.save(cat);
 
         Owner owner1= new Owner();
         owner1.setFirstName("Michael");
