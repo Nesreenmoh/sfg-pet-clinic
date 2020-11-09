40402c0a1750de215fecda31b401164c0850509e
Nesreen <nesreenmhd@gmail.com>
2020-11-09 13:44:03 +0100

    Create Spring Data JPA Service for Vets. Closes #42.

---
+++ pet-clinic-data/src/main/java/com/capgemini/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,51 @@
+package com.capgemini.services.springdatajpa;
+
+import com.capgemini.models.Vet;
+import com.capgemini.repositories.SpecialityRepository;
+import com.capgemini.repositories.VetRepository;
+import com.capgemini.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+    private final SpecialityRepository specialityRepository;
+
+    public VetSDJpaService(VetRepository vetRepository, SpecialityRepository specialityRepository) {
+        this.vetRepository = vetRepository;
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vetSet = new HashSet<>();
+        vetRepository.findAll().forEach(vetSet::add);
+        return vetSet;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+}
