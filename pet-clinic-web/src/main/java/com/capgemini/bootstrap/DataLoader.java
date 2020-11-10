696496f59533e228f70e113f8b068a554e0d5cbf
Nesreen <nesreenmhd@gmail.com>
2020-11-10 18:55:08 +0100

    Upgrade to Junit 5. Closes #4.

--- pet-clinic-web/src/main/java/com/capgemini/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/capgemini/bootstrap/DataLoader.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
---
+++ pet-clinic-web/src/test/java/com/capgemini/SfgPetClinicApplicationTest.java
@@ -0,0 +1,23 @@
+package com.capgemini;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTest {
+
+    @Test
+     public void contextLoads(){
+
+     }
+    @Test
+    public void main(){
+
+    }
+
+}
