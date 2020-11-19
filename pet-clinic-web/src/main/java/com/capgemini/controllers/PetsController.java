251eb93a6be02d92527ff1f18006e70d32984863
Nesreen <nesreenmhd@gmail.com>
2020-11-19 11:25:12 +0100

    Add ability to add or update a Pet. Closes #58.

--- pet-clinic-web/src/main/java/com/capgemini/controllers/PetsController.java
+++ pet-clinic-web/src/main/java/com/capgemini/controllers/PetsController.java
@@ -1,14 +1,43 @@
 package com.capgemini.controllers;
 
+import com.capgemini.models.Owner;
+import com.capgemini.models.PetType;
+import com.capgemini.services.OwnerService;
+import com.capgemini.services.PetTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/pets")
+@RequestMapping("/owner/{ownerId}")
 public class PetsController {
+
+    private final static String PETS_CREATE_OR_UPDATE="pets/createorupdatepets";
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
+
+
+    public PetsController(PetTypeService petTypeService, OwnerService ownerService) {
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"","/","index","index.html"})
     public String petsList(){
         return "pets/index";
+    }
+
+
+    @ModelAttribute("types")
+    public List<PetType> populatePetTypes(){
+        return this.petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId){
+        return ownerService.findById(ownerId);
     }
 }
