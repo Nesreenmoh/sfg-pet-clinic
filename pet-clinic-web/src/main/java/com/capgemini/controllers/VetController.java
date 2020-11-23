5138b920d43003e38e24b8f2a7f70ed92e99ee58
Nesreen Al-Shargabi <nesreenmhd@gmail.com>
2020-11-23 10:35:46 +0100

    Implements Vets JSON endpoint. Closes #72.

--- pet-clinic-web/src/main/java/com/capgemini/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/capgemini/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.capgemini.controllers;
 
+import com.capgemini.models.Vet;
 import com.capgemini.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/vets")
@@ -17,10 +22,18 @@ public class VetController {
     }
 
 
+
     @RequestMapping({"","/","index","index.html"})
     public String listVets(Model model)
     {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    // visit this localhost:8080/vets/api/vets to show JSON of vets
+
+    @GetMapping("/api/vets")
+    public @ResponseBody  List<Vet> getVetsJSON(){
+        return vetService.findAll();
     }
 }
